52a86e6e105ba605fbd52ccfdb42b34bd0fe590e
MAskari22 <maskari.bscs22seecs@seecs.edu.pk>
2024-03-02 10:24:25 +0500

    post

---
+++ server/models/post.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: [
+      {
+        type: { type: String },
+        attachment: String,
+        content: String,
+      },
+    ],
+    default: [],
+  },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
