0545d475aa983dd0d76ef471c717109bcd02a042
Taha Shah <tahashah411@gmail.com>
2024-03-02 11:40:18 +0500

    bypass auth

--- client/components/Authenticator.jsx
+++ client/components/Authenticator.jsx
@@ -1,18 +1,26 @@
 "use client"
 
-import { useAppSelector } from "@/lib/hooks"
+import { setUser } from "@/lib/auth/authSlice"
+import { useAppDispatch, useAppSelector } from "@/lib/hooks"
 import { usePathname, useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 export const Authenticator = ({ children }) => {
   const { user } = useAppSelector(state => state.auth)
+  const dispatch = useAppDispatch()
   const router = useRouter()
   const path = usePathname()
 
   useEffect(() => {
-    if (!user && path !== "/login" && path !== "/signup") {
-      router.push("/login")
-    }
+    // if (!user && path !== "/login" && path !== "/signup") {
+    //   router.push("/login")
+    // }
+    if (!user) dispatch(setUser({
+      name: "FakeShah141",
+      password: "FakeBoi@123",
+      email: "fakeshah141@test.com",
+      avatarLink: "#"
+    }))
   }, [user, path])
 
   return (
